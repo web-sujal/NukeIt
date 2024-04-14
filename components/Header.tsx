05f4f751172692a28151c046ff3d8164d53e90f9
Sujal <web.sujal@gmail.com>
2024-04-14 23:16:53 +0530

    styles: remove menu button on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,15 +5,10 @@ import { MdOutlineMenu } from "react-icons/md";
 
 const Header = () => {
   return (
-    <h1 className="flex w-full items-center justify-between pb-6 text-start text-3xl font-extrabold">
+    <h1 className="flex w-full items-center justify-start pb-6 text-start text-3xl font-extrabold">
       <Link href="/" className="cursor-pointer">
         ❄️ Nuke<span className="text-primary dark:text-secondary">It</span>
       </Link>
-
-      {/* TODO: add modal of sidebar */}
-      <button onClick={() => alert("clicked")} className="lg:hidden">
-        <MdOutlineMenu className="text-primary dark:text-secondary" size={40} />
-      </button>
     </h1>
   );
 };
