1b4a0b7dbbfab2b6483ff9db0999550342d37fe3
Sujal <web.sujal@gmail.com>
2024-04-22 09:12:22 +0530

    remove comments for better readability

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -46,32 +46,3 @@ const config: Config = {
 };
 
 export default config;
-
-/*
-  colors: {
-        // Primary color
-        primary: "#F06666",
-        "primary-dark": "#ee5353",
-        "primary-darker": "#ec4141",
-
-        // Secondary color, used in dark mode as primary
-        secondary: "#8564F7",
-        "secondary-dark": "#7046f6",
-        "secondary-darker": "#6133f5",
-
-        // Text color
-        "primary-heading": "#7A89A3", // just for headings
-        "secondary-heading": "#A1A5F3", // just for headings
-        "secondary-subheading": "#81819A", // just for headings
-
-        // Background colors
-        light: "#F6F2F3", // Background color with primary
-        dark: "#121215", // Background color with secondary
-
-        // Modals
-        "modal-primary": "#FEFCFD",
-        "modal-secondary": "#18181C",
-        "modal-highlight-primary": "#D3DDE2",
-        "modal-highlight-secondary": "#2F2D36",
-      },
-*/
