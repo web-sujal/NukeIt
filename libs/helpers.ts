11f0647e3129644584104c54e343bdabd543c09d
Sujal <web.sujal@gmail.com>
2024-05-21 14:08:41 +0530

    add getTaskTypeFromRoute() helper fn

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -1,3 +1,5 @@
+import { TaskType } from "@/types";
+
 export function getCurrentTime(): string {
   const d = new Date();
   const fullTime = d.toLocaleTimeString();
@@ -13,4 +15,23 @@ export function convertTo12HourFormat(time: string): string {
   const hour = +hours % 12 || 12; // Convert '0' to '12'
   const period = +hours < 12 ? "AM" : "PM";
   return `${hour.toString().padStart(2, "0")}:${minutes} ${period}`;
+}
+
+export function getTaskTypeFromRoute(pathName: string): TaskType {
+  switch (pathName) {
+    case "/":
+      return "daily";
+
+    case "/this-week":
+      return "weekly";
+
+    case "/this-month":
+      return "monthly";
+
+    case "/misc":
+      return "misc";
+
+    default:
+      return "daily";
+  }
 }
