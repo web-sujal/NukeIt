ec1c115256ed19cd1d0249f89858e74413d83aa6
Sujal <web.sujal@gmail.com>
2024-05-23 01:57:00 +0530

    refactor: remove getColor and getDotColor functions
    Removed getColor and getDotColor functions from the helpers.ts file as they are now locally defined where needed.

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -1,4 +1,4 @@
-import { TaskStatus, TaskType } from "@/types";
+import { TaskType } from "@/types";
 
 export function getCurrentTime(): string {
   const d = new Date();
@@ -35,35 +35,3 @@ export function getTaskTypeFromRoute(pathName: string): TaskType {
       return "daily";
   }
 }
-
-export const getColor = (status: TaskStatus): string => {
-  switch (status) {
-    case "completed":
-      return "bg-green-700 dark:bg-green-800";
-    case "haven't done":
-      return "bg-red-700 dark:bg-red-800";
-    case "tomorrow":
-      return "bg-orange-700 dark:bg-orange-800";
-    case "not started":
-      return "bg-gray-700 dark:bg-gray-800";
-    case "in progress":
-    default:
-      return "bg-blue-700 dark:bg-blue-800";
-  }
-};
-
-export const getDotColor = (status: TaskStatus): string => {
-  switch (status) {
-    case "completed":
-      return "text-green-400";
-    case "haven't done":
-      return "text-red-400";
-    case "tomorrow":
-      return "text-orange-400";
-    case "not started":
-      return "text-gray-400";
-    case "in progress":
-    default:
-      return "text-blue-400";
-  }
-};
