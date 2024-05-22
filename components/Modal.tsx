d17d87707a9800e21f66a4aca39e312f359885fa
Sujal <web.sujal@gmail.com>
2024-05-23 01:41:44 +0530

    style: update Modal UI for mobile screens
    Updated the Modal component's UI styles to improve appearance on mobile screens and made the title and description optional.

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,13 +1,14 @@
-import useTheme from "@/hooks/useTheme";
 import * as Dialog from "@radix-ui/react-dialog";
 import { IoMdClose } from "react-icons/io";
 import { twMerge } from "tailwind-merge";
 
+import useTheme from "@/hooks/useTheme";
+
 interface ModalProps {
   isOpen: boolean;
   onChange: (open: boolean) => void;
-  title: string;
-  description: string;
+  title?: string;
+  description?: string;
   children: React.ReactNode;
 }
 
@@ -28,20 +29,24 @@ const Modal: React.FC<ModalProps> = ({
 
         <Dialog.Content
           className={twMerge(
-            "fixed left-[50%] top-[50%] h-full max-h-full w-full translate-x-[-50%] translate-y-[-50%] rounded-md border border-neutral-700 bg-neutral-800 p-[25px] text-white drop-shadow-md focus:outline-none md:h-auto md:max-h-[85vh] md:w-[95vw] md:max-w-[500px]",
+            "fixed left-[50%] top-[50%] flex h-full max-h-full w-full translate-x-[-50%] translate-y-[-50%] flex-col items-center justify-center rounded-md border border-neutral-700 bg-neutral-800 p-[25px] text-white drop-shadow-md focus:outline-none md:h-auto md:max-h-[85vh] md:w-[95vw] md:max-w-[500px]",
             isDarkMode === false &&
               "border-modal-primary bg-modal-primary text-primary-heading",
           )}
         >
-          <Dialog.Title className="mb-4 text-center text-xl font-bold">
-            {title}
-          </Dialog.Title>
+          {title && (
+            <Dialog.Title className="mb-4 text-center text-xl font-bold">
+              {title}
+            </Dialog.Title>
+          )}
 
-          <Dialog.Description className="mb-5 text-center text-sm leading-normal">
-            {description}
-          </Dialog.Description>
+          {description && (
+            <Dialog.Description className="mb-5 text-center text-sm leading-normal">
+              {description}
+            </Dialog.Description>
+          )}
 
-          <div>{children}</div>
+          <div className="w-full">{children}</div>
 
           <Dialog.Close asChild>
             <button className="absolute right-[10px] top-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full text-neutral-400 hover:text-white focus:outline-none">
