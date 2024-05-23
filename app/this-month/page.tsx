62b828779a95d63c7cd96fc9b6de777f5adb44b1
Sujal <web.sujal@gmail.com>
2024-05-23 18:21:02 +0530

    refactor(pages): fully functional data rendering for misc, this-month, and this-week
    - Removed hard-coded code and made pages fully functional to render appropriate data

--- app/this-month/page.tsx
+++ app/this-month/page.tsx
@@ -1,12 +1,43 @@
+"use client";
+
 import Box from "@/components/Box";
 import NothingHere from "@/components/NothingHere";
+import Task from "@/components/Task";
 import Title from "@/components/Title";
+import useTaskStore from "@/hooks/useTaskStore";
+import Loader from "@/components/Loader";
 
 const ThisMonth = () => {
+  const { isLoading, getTasksByType } = useTaskStore();
+  const monthlyTasks = getTasksByType("monthly");
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <Box>
       <Title label="Monthly Goals" />
-      <NothingHere />
+
+      {monthlyTasks.length ? (
+        <div className="md:scrollbar no-scrollbar flex h-full w-full flex-1 flex-col gap-y-3 overflow-y-auto pt-2">
+          {monthlyTasks.map((task) => (
+            <Task
+              key={task.id}
+              title={task.title}
+              type={task.type}
+              status={task.status}
+              start_time={task.start_time}
+              end_time={task.end_time}
+              alarm={task.alarm}
+              desc={task.desc}
+              priority={task.priority}
+            />
+          ))}
+        </div>
+      ) : (
+        <NothingHere />
+      )}
     </Box>
   );
 };
--- app/this-week/page.tsx
+++ app/this-week/page.tsx
@@ -1,12 +1,43 @@
+"use client";
+
 import Box from "@/components/Box";
+import Loader from "@/components/Loader";
 import NothingHere from "@/components/NothingHere";
+import Task from "@/components/Task";
 import Title from "@/components/Title";
+import useTaskStore from "@/hooks/useTaskStore";
 
 const ThisWeek = () => {
+  const { isLoading, getTasksByType } = useTaskStore();
+  const weeklyTasks = getTasksByType("weekly");
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <Box>
       <Title label="Weekly Goals" />
-      <NothingHere />
+
+      {weeklyTasks.length ? (
+        <div className="md:scrollbar no-scrollbar flex h-full w-full flex-1 flex-col gap-y-3 overflow-y-auto pt-2">
+          {weeklyTasks.map((task) => (
+            <Task
+              key={task.id}
+              title={task.title}
+              type={task.type}
+              status={task.status}
+              start_time={task.start_time}
+              end_time={task.end_time}
+              alarm={task.alarm}
+              desc={task.desc}
+              priority={task.priority}
+            />
+          ))}
+        </div>
+      ) : (
+        <NothingHere />
+      )}
     </Box>
   );
 };
