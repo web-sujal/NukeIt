e06810c9fe7997d5a9e409081fd474e8739f3dbe
Sujal <web.sujal@gmail.com>
2024-05-24 00:41:22 +0530

    refactor(useTaskForm): improve readability and maintainability, add initialize action

--- hooks/useTaskForm.ts
+++ hooks/useTaskForm.ts
@@ -1,27 +1,42 @@
 import { create } from "zustand";
 
-import { Priority, TaskStatus, TaskType } from "@/types";
+import { Priority, Task, TaskStatus, TaskType } from "@/types";
 
-interface useTaskFormStore {
+interface State {
   status: TaskStatus;
   priority: Priority;
   type: TaskType;
   alarmStatus: boolean;
+}
+
+interface Actions {
   setStatus: (status: TaskStatus) => void;
   setPriority: (priority: Priority) => void;
   setType: (type: TaskType) => void;
   setAlarmStatus: (status: boolean) => void;
+  initialize: (task: Partial<Task>) => void;
 }
 
-const useTaskForm = create<useTaskFormStore>((set) => ({
+const initial_state: State = {
   status: "not started",
   priority: "low",
   type: "daily",
   alarmStatus: false,
+};
+
+const useTaskForm = create<State & Actions>((set) => ({
+  ...initial_state,
   setStatus: (status) => set({ status }),
   setPriority: (priority) => set({ priority }),
   setType: (type) => set({ type }),
   setAlarmStatus: (alarmStatus) => set({ alarmStatus }),
+  initialize: (task) =>
+    set({
+      status: task.status || initial_state.status,
+      priority: task.priority || initial_state.priority,
+      type: task.type || initial_state.type,
+      alarmStatus: task.alarm || initial_state.alarmStatus,
+    }),
 }));
 
 export default useTaskForm;
